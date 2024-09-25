75859721e74e9a4ef7188789952e1e46745ecb7f
vamshi <panja.vamshi2023@gmail.com>
2024-09-25 13:30:25 -0500

    added conversation script

--- 02-conversation.sh
+++ 02-conversation.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-echo "vasu:: hi suresh
-echo "suresh:: hello vasu.how are you?"
-echo "vasu:: i am good i started learning shell script suresh"
+echo "Ramesh:: Hi suresh, how are you?"
+echo "suresh:: hello ramesh.I am fine. how are you doing?"
+echo "Ramesh:: I am doing good suresh. waht's going on?"
+echo "Suresh:: I started learning  shell script ramesh"
