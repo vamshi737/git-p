fe93cbc717fc37ba0ab3dba568b5ca1b91871589
vamshi <panja.vamshi2023@gmail.com>
2024-09-26 11:23:31 -0500

    variables

---
+++ 04-variables.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "please enter your username::"
+
+read -s USERNAME #takes input into username variable
+
+echo "username entered is: $USERNAME"
+
+echo "please enter your password::"
+
+read -s PASSWORD
+
+echo "password enterd is:$PASSWORD
