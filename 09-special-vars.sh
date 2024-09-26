5caa2c1d827d2b8c886849c57a4ee1af1ce15293
vamshi <panja.vamshi2023@gmail.com>
2024-09-26 13:55:27 -0500

    variables

--- 09-special-vars.sh
+++ 09-special-vars.sh
@@ -2,3 +2,9 @@
 
 echo "All variables passed to the script: $@"
 echo "number of variables passed: $#"
+echo "script name:$0"
+echo "current working directory: $PWD"
+echo "home directory of current user: $HOME"
+echo "PID of the script executing now: $$"
+sleep 100 &
+echo "PID of last background command: $!"
---
+++ 10-conditions.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+number=$1
+
+if [ $NUMBER -GT 20 ]
+then 
+     echo "given number: $number is greater than 20"
+    else
+     echo "given number: $number is less than 20"
+     fi
