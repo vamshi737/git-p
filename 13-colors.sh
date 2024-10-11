af4b0c8248a0be34877a8f2caf7e1def50ca8fba
vamshi <panja.vamshi2023@gmail.com>
2024-10-11 08:11:23 -0500

    variables

--- 13-colors.sh
+++ 13-colors.sh
@@ -16,10 +16,10 @@ CHECK_ROOT (){
 VALIDATE(){
    if [ $1 -ne 0 ]
    then
-       echo -e "$2 is...$R FAILED$N"
+       echo -e "$2 is...$R FAILED $N"
        exit 1
   else 
-      echo "-e "$2 is...$G SUCCESS"$N"
+      echo "-e "$2 is...$G SUCCESS $N"
     fi
 }
 
