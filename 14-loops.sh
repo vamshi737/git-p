e9355a62be75cebde66c177f38f3a39cedb860bd
vamshi <panja.vamshi2023@gmail.com>
2024-10-11 08:35:49 -0500

    variables

--- 14-loops.sh
+++ 14-loops.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
-echo "1"
-echo "2"
-echo "3"
-echo "4"
-echo "5"
-echo "6"
+for i in 1 2 3 4 5 6 
+do 
+  echo $i
+done
+
+
+
+
+# echo "1"
+# echo "2"
+# echo "3"
+# echo "4"
+# echo "5"
+# echo "6"
+
