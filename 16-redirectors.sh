13d45a0f0ce2241f96aa480359f27ab38a875167
vamshi <panja.vamshi2023@gmail.com>
2024-10-14 20:55:17 -0500

    variables

--- 16-redirectors.sh
+++ 16-redirectors.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+LOGS_FOLDER="/var/log/shell-script"
+SCRIPT_NAME=$(echo $0 |cut -d "." -f1)
+TIMESTAMP=$*(date +%y-%m-%d-%-H-%M-%-S)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME-$TIMESTAMP.log"
+mkdir -p $LOGS_FOLDER
+
 USERID=$(id -u)
 R=" \e[31m"
 G=" \e[32m"
@@ -8,7 +14,7 @@ G=" \e[30m"
 CHECK_ROOT (){
     if [ $USERID -ne 0 ]
     then 
-       echo "please run this script with root priveleges"
+       echo  -e "$R please run this script with root priveleges $N" &>>$LOG_FILE
        exit 1
     fi 
 }
@@ -16,10 +22,10 @@ CHECK_ROOT (){
 VALIDATE(){
    if [ $1 -ne 0 ]
    then
-       echo -e "$2 is...$R FAILED $N"
+       echo -e "$2 is...$R FAILED $N" &>>$LOG_FILE
        exit 1
   else 
-      echo -e "$2 is...$G SUCCESS $N"
+      echo -e "$2 is...$G SUCCESS $N" &>>$LOG_FILE
     fi
 }
 
